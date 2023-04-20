cc56f0b8c743016fadf0b344d6d89bfdf309573c
Daniele Cortesi <dani.c93@hotmail.it>
2023-04-20 22:00:37 +0200

    Improve dragon movement

--- Assets/Project/Character/DragonMovement.cs
+++ Assets/Project/Character/DragonMovement.cs
@@ -1,3 +1,4 @@
+using JK.Utils;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -17,29 +18,34 @@ namespace Project.Character
         public float speed = 5.0f;
         public float lerpSpeed = 0.15f;
 
-        private Vector3 moveDirection;
+        [RuntimeField]
+        public Vector3 moveDirection;
+
+        [RuntimeField]
+        public Vector3 velocity;
 
         private void Reset()
         {
             rb = GetComponent<Rigidbody>();
         }
+
         #endregion 
 
-        private void Update()
+        private void Start()
+        {
+            moveDirection = Vector3.zero;
+            velocity = Vector3.zero;
+        }
+
+        private void FixedUpdate()
         {
             float horizontal = Input.GetAxis("Horizontal");
             float vertical = Input.GetAxis("Vertical");
 
             moveDirection = new Vector3(horizontal, 0, vertical);
-
-            moveDirection = moveDirection.normalized * speed;
-
-        }
-
-        private void FixedUpdate()
-        {
-
-            rb.velocity = Vector3.Lerp(rb.velocity, moveDirection, lerpSpeed);
+            moveDirection = Vector3.ClampMagnitude(moveDirection, 1) * speed;
+            velocity = Vector3.Lerp(velocity, moveDirection, lerpSpeed);
+            rb.velocity = velocity;
         }
     }
 }
