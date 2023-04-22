c1a54a28bb4e259f599dd9bd2230c991e0712650
Daniele Cortesi <dani.c93@hotmail.it>
2023-04-22 21:47:34 +0200

    Prepare for controlling brazier fire

--- Assets/Project/Cooking/Cooking.cs
+++ Assets/Project/Cooking/Cooking.cs
@@ -14,14 +14,49 @@ namespace Project.Cooking
         #region Inspector
 
         public float cookingTime = 10f;
+
+        public ParticleSystem particles;
+        public ParticleSystem smokeParticles;
+
         public Slider slider;
+
+        [ContextMenu("Stop")]
+        private void StopInEditMode()
+        {
+            if (Application.isPlaying)
+                particles.Stop();
+        }
+
+        [ContextMenu("Start smoke")]
+        private void StartSmokeInEditMode()
+        {
+            if (Application.isPlaying)
+            {
+                smokeParticles.gameObject.SetActive(true);
+                smokeParticles.Play();
+            }
+        }
+
+        [ContextMenu("Stop smoke")]
+        private void StopSmokeInEditMode()
+        {
+            if (Application.isPlaying)
+                smokeParticles.Stop();
+        }
+
 
         #endregion
 
         public void StartCooking()
         {
+            particles.gameObject.SetActive(true);
             slider.transform.DOScale(1, 1f).SetEase(Ease.OutBounce);
             slider.DOValue(1f, cookingTime).SetEase(Ease.Linear);
+        }
+
+        public void StopCooking()
+        {
+            particles.Stop();
         }
     }
 }
