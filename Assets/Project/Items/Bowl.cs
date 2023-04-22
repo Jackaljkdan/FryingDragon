a2bb4aff99f4318b8e12c98a7c711e1987b68b05
spaghetticoder <spaghetticoder101@gmail.com>
2023-04-22 09:32:25 +0200

    Bowl spawn logic updated

--- Assets/Project/Items/Bowl.cs
+++ Assets/Project/Items/Bowl.cs
@@ -1,3 +1,4 @@
+using JK.Injection;
 using JK.Utils;
 using System;
 using System.Collections;
@@ -13,16 +14,42 @@ namespace Project.Items
         #region Inspector
 
         public Transform spawnAnchor;
+        public Rigidbody rb;
 
         [RuntimeField]
         public List<GameObject> ingredients = new();
 
+        [Injected]
+        public Transform anchorTransform;
+
+        private void Reset()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
         #endregion
+
+        [InjectMethod]
+        public void Inject()
+        {
+            Context context = Context.Find(this);
+            anchorTransform = context.Get<Transform>(this, "dragon.anchor");
+        }
+
+        private void Awake()
+        {
+            Inject();
+        }
+
+        private void FixedUpdate()
+        {
+            rb.MovePosition(anchorTransform.position);
+        }
 
         public void TryAddIngredient(GameObject ingredient)
         {
             ingredients.Add(ingredient);
-            Instantiate(ingredient, spawnAnchor.position, spawnAnchor.rotation);
+            Instantiate(ingredient, spawnAnchor.position, spawnAnchor.rotation.normalized, transform.root);
         }
     }
 }
