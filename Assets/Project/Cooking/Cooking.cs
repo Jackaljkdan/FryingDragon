75e25ab1e36d19c37a5c0ef51aa1e23f941fa410
spaghetticoder <spaghetticoder101@gmail.com>
2023-04-24 10:40:20 +0200

    Added overcooking

--- Assets/Project/Cooking/Cooking.cs
+++ Assets/Project/Cooking/Cooking.cs
@@ -18,8 +18,15 @@ namespace Project.Cooking
 
         public ParticleSystem particles;
         public ParticleSystem smokeParticles;
+        public ParticleSystem readyParticles;
 
         public Slider slider;
+        public Image sliderBackgroundArea;
+        public Image sliderFillArea;
+
+        public Color backgroundColor = new Color(63, 63, 63);
+        public Color cookingColor = Color.white;
+        public Color overcookingColor = Color.red;
 
         [ContextMenu("Stop")]
         private void StopInEditMode()
@@ -54,14 +61,43 @@ namespace Project.Cooking
 
         public void StartCooking()
         {
+            readyParticles.gameObject.SetActive(false);
+            SetCookingColors();
             particles.gameObject.SetActive(true);
             slider.transform.DOScale(1, 1f).SetEase(Ease.OutBounce);
             cookingTween = slider.DOValue(1f, cookingTime).SetEase(Ease.Linear);
+
+            cookingTween.OnComplete(() =>
+            {
+                readyParticles.gameObject.SetActive(true);
+                readyParticles.Play();
+                SetOvercookingColors();
+                slider.DOValue(1f, cookingTime).SetEase(Ease.Linear);
+            });
+        }
+
+        public void StartOvercooking()
+        {
+
         }
 
         public void StopCooking()
         {
             particles.Stop();
+        }
+
+        private void SetCookingColors()
+        {
+            slider.value = 0;
+            sliderBackgroundArea.color = backgroundColor;
+            sliderFillArea.color = cookingColor;
+        }
+
+        private void SetOvercookingColors()
+        {
+            slider.value = 0;
+            sliderBackgroundArea.color = cookingColor;
+            sliderFillArea.color = overcookingColor;
         }
     }
 }
