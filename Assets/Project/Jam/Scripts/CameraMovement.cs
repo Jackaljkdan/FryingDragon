d603f36498336ce764d63082f1cc2c55fda6975c
spaghetticoder <spaghetticoder101@gmail.com>
2023-04-30 10:27:00 +0200

    Camera now also follows firefighter

--- Assets/Project/Jam/Scripts/CameraMovement.cs
+++ Assets/Project/Jam/Scripts/CameraMovement.cs
@@ -1,4 +1,7 @@
+using JK.Injection;
 using JK.Utils;
+using Project.Dragon;
+using Project.Flames;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -19,22 +22,78 @@ namespace Project.Jam
         [DebugField]
         public Vector3 offset;
 
+        [DebugField]
+        public float cameraY;
+
+        [Injected]
+        public Transform dragonTransform;
+
+        [Injected]
+        private SignalBus signalBus;
+
+        [InjectMethod]
+        public void Inject()
+        {
+            Context context = Context.Find(this);
+            dragonTransform = context.Get<Transform>(this, "dragon");
+            signalBus = context.Get<SignalBus>(this);
+        }
+
         #endregion
 
         private void Awake()
         {
-            if (target != null)
-            {
-                offset = transform.position - target.position;
-            }
+            Inject();
         }
+
+        private void Start()
+        {
+            cameraY = transform.position.y;
+
+            signalBus.AddListener<FirefighterSpawnSignal>(OnFirefighterSpawned);
+            signalBus.AddListener<FirefighterExitSignal>(OnFirefighterExited);
+
+            if (target)
+                return;
+
+            SetTarget(dragonTransform);
+            SetOffset();
+        }
+
+        private void OnDestroy()
+        {
+            signalBus.RemoveListener<FirefighterSpawnSignal>(OnFirefighterSpawned);
+            signalBus.RemoveListener<FirefighterExitSignal>(OnFirefighterExited);
+        }
+
+        private void SetOffset()
+        {
+            offset = transform.position - target.position;
+        }
+
+        private void SetTarget(Transform newTarget)
+        {
+            target = newTarget;
+        }
+
+        private void OnFirefighterSpawned(FirefighterSpawnSignal signal)
+        {
+            SetTarget(signal.firefighter.transform);
+        }
+
+        private void OnFirefighterExited(FirefighterExitSignal signal)
+        {
+            SetTarget(dragonTransform);
+        }
+
         private void LateUpdate()
         {
-            if (target == null) return;
+            if (target == null)
+                return;
 
             Vector3 desiredPosition = target.position + offset;
             desiredPosition.z = Mathf.Max(desiredPosition.z, minZPosition);
-            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition.WithY(cameraY), smoothSpeed);
             transform.position = smoothedPosition;
         }
     }
