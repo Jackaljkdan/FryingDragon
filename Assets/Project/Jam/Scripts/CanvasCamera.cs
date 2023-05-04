7d3c08302a259f85d65276e7ee1d7d31e05810b7
Daniele Cortesi <dani.c93@hotmail.it>
2023-05-04 16:20:42 +0200

    Prepare canvas for tiktok

---
+++ Assets/Project/Jam/Scripts/CanvasCamera.cs
@@ -0,0 +1,45 @@
+using JK.Injection;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Project.Jam
+{
+    [DisallowMultipleComponent]
+    public class CanvasCamera : MonoBehaviour
+    {
+        #region Inspector
+
+        public Canvas canvas;
+
+        public float planeDistance = 1;
+
+        public string injectionId = "ui";
+
+        [Injected]
+        public new Camera camera;
+
+        private void Reset()
+        {
+            canvas = GetComponent<Canvas>();
+        }
+
+        #endregion
+
+        [InjectMethod]
+        public void Inject()
+        {
+            Context context = Context.Find(this);
+            camera = context.Get<Camera>(this, injectionId);
+        }
+
+        private void Awake()
+        {
+            Inject();
+            canvas.worldCamera = camera;
+            canvas.planeDistance = planeDistance;
+        }
+    }
+}
