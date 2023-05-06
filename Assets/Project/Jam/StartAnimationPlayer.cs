1694946921bcc0884ef30173f08380263e7a275e
spaghetticoder <spaghetticoder101@gmail.com>
2023-05-06 12:10:13 +0200

    Added Animation selector and autoplay from editor

---
+++ Assets/Project/Jam/StartAnimationPlayer.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Project.Jam
+{
+    [DisallowMultipleComponent]
+    public class StartAnimationPlayer : MonoBehaviour
+    {
+        #region Inspector
+
+        public Animator animator;
+        public string selectedAnimation;
+
+        #endregion
+
+        private void Start()
+        {
+            PlaySelectedAnimation();
+        }
+
+        public void PlaySelectedAnimation()
+        {
+            if (animator != null && !string.IsNullOrEmpty(selectedAnimation))
+            {
+                animator.Play(selectedAnimation);
+            }
+        }
+
+    }
+}
