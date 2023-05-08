995ca76a6d5576a7db14404a8fd7603a0413a0a2
spaghetticoder <spaghetticoder101@gmail.com>
2023-05-08 16:08:02 +0200

    Added TargetLandingVisualizer script and prefab

---
+++ Assets/Project/Jam/Scripts/TargetLandingVisualizer.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Project.Jam
+{
+    [DisallowMultipleComponent]
+    public class TargetLandingVisualizer : MonoBehaviour
+    {
+        #region Inspector
+
+        public LayerMask hitLayer;
+        public GameObject hitObjectPrefab;
+
+        #endregion
+
+        private GameObject instatiatedPrefab;
+
+        private void Update()
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, Vector3.down, out hit, Mathf.Infinity, hitLayer))
+            {
+
+                if (!instatiatedPrefab)
+                    instatiatedPrefab = Instantiate(hitObjectPrefab, hit.point, Quaternion.identity, transform.root);
+
+                instatiatedPrefab.transform.position = hit.point;
+
+            }
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (!instatiatedPrefab)
+                return;
+
+            Destroy(instatiatedPrefab);
+            this.enabled = false;
+        }
+    }
+}
