b5d90d5c4203b4f70d725787a71be2b52f98f7e5
Daniele Cortesi <dani.c93@hotmail.it>
2023-05-08 22:13:01 +0200

    Add input list logic

---
+++ Assets/Project/UI/InputEntry.cs
@@ -0,0 +1,22 @@
+using JK.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Project.UI
+{
+    [DisallowMultipleComponent]
+    public class InputEntry : MonoBehaviour
+    {
+        #region Inspector
+
+        public ButtonIcon icon;
+
+        public TMP_Text text;
+
+        #endregion
+    }
+}
